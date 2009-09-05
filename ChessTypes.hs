8e462f0199c3a520e290d70364d12ef51501fa77
Tito Nguyen <nguyentito@gmail.com>
2009-09-05 18:12:10 +0200

    Completed type declarations (castling is now an alternative to normal chess moves, some ADTs have automatically derived instances of Eq and Show) ; PNG images of chess pieces have been added.

--- ChessTypes.hs
+++ ChessTypes.hs
@@ -5,15 +5,21 @@ import Data.Map (Map)
 type Square = (Int, Int)
 
 data Piece = Piece PieceType Color
+             deriving (Eq)
 data PieceType = King | Queen | Rook | Bishop | Knight | Pawn
+               deriving (Eq, Show)
 data Color = Black | White
+           deriving (Eq, Show)
 
 data Position = Position Board GameState
 type Board = Map Square Piece
 data GameState = GameState () -- may be used for castling and/or en passant
 
-data Move = Move {
-      movePiece :: PieceType,
-      moveOrig :: Square,
-      moveDest :: Square
-    }
+boardToPosition = flip Position (GameState ())
+
+data Move = StandardMove { movePiece :: PieceType,
+                           moveOrig :: Square,
+                           moveDest :: Square
+                         }
+          | KingsideCastling
+          | QueensideCastling
