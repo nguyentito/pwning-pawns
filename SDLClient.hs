3f3bebdaee9bd442b162000adfef1fa7c240608d
Tito Nguyen <nguyentito@gmail.com>
2009-09-05 18:54:40 +0200

    Changes in this commit : * Refactored the code to use the Reader Monad * The SDL window draws the starting position of a chess game

--- SDLClient.hs
+++ SDLClient.hs
@@ -1,33 +1,64 @@
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Reader
+import Data.Char
+import Data.Map (Map)
+import qualified Data.Map as M
+import qualified Data.Foldable as F
 
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as IMG
 
-squareSidePx = 72
+import ChessTypes
+import Common
+
+squareSidePx = 96
 boardSidePx = squareSidePx * 8
+
+data AppData = AppData { screen :: SDL.Surface, piecesImagesMap :: Map Piece SDL.Surface }
 
 main = SDL.withInit [SDL.InitVideo] $ do
   screen <- SDL.setVideoMode boardSidePx boardSidePx 0 [SDL.HWSurface, SDL.DoubleBuf]
-  mainLoop screen
+  piecesImagesMap <- loadPiecesImages
+  runReaderT mainLoop (AppData screen piecesImagesMap)
   
-mainLoop screen = do
-  events <- pollEvents
+mainLoop = do
+  events <- liftIO pollEvents
   unless (SDL.Quit `elem` events) $ do
-    drawBoard screen
-    SDL.flip screen
-    mainLoop screen
+    drawBoard
+    drawPosition startingPosition
+    (liftIO . SDL.flip) =<< asks screen
+    mainLoop
 
-drawBoard screen = mapM_ (drawSquare screen) [(x, y) | x <- [0..7], y <- [0..7]]
+drawBoard = sequence_ [drawSquare x y | x <- [0..7], y <- [0..7]]
+drawSquare x y = do
+  screen <- asks screen
+  liftIO $ do
+    let toPixel = SDL.mapRGB (SDL.surfaceGetPixelFormat screen)
+    color <- if (x + y) `mod` 2 == 0
+             then toPixel 255 255 255
+             else toPixel 128 128 255
+    SDL.fillRect screen
+                 (Just $ SDL.Rect (x*squareSidePx) (y*squareSidePx) squareSidePx squareSidePx) 
+                 color
 
-drawSquare screen (x, y) = do
-  let toPixel = SDL.mapRGB (SDL.surfaceGetPixelFormat screen)
-  color <- if (x + y) `mod` 2 == 0
-           then toPixel 255 255 255
-           else toPixel 128 128 255
-  SDL.fillRect screen
-               (Just $ SDL.Rect (x*squareSidePx) (y*squareSidePx) squareSidePx squareSidePx) 
-               color
+drawPosition (Position piecesMap _) = drawPieces piecesMap
+drawPieces = F.sequence_ . M.mapWithKey drawPiece
+drawPiece (col, row) piece = do
+  img <- (M.! piece) <$> asks piecesImagesMap
+  screen <- asks screen
+  liftIO $ SDL.blitSurface img Nothing screen (Just $ SDL.Rect x y 0 0)
+    where x = (col - 1) * squareSidePx
+          y = (8 - row) * squareSidePx
+     
+
+loadPiecesImages = M.fromList <$> mapM loadPieceImage pieces
+  where pieceTypes = [King, Queen, Rook, Bishop, Knight, Pawn]
+        pieces = [Piece pieceType color | color <- [Black, White], pieceType <- pieceTypes]
+        loadPieceImage piece@(Piece pieceType color) =
+            ((,) piece) <$> (IMG.load $ "img/" ++
+                                        map toLower (show color) ++ "." ++
+                                        map toLower (show pieceType) ++ ".png")
 
 pollEvents = SDL.pollEvent >>= recur
     where recur SDL.NoEvent = return []
