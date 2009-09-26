463e5b623aa48fc099306c4cc07d9c21c90b2147
Tito Nguyen <nguyentito@gmail.com>
2009-09-26 13:42:57 +0200

    A part of the client, and the entire server, were rewritten.
    * The client now uses WxHaskell, and will eventually allow you to create one or multiple games, choose your color, and join other games all from a graphical interface;
    * The server used to test networking was thrown away and a new one is being written to implement the features cited above;
    * There is already a working prototype which only tests basic communication.

--- NetworkClient.hs
+++ NetworkClient.hs
@@ -1,14 +1,97 @@
+import Control.Applicative
 import Control.Concurrent
 import Control.Monad
 import Data.List
+import Data.Maybe
 import System.IO
 import Network
+
+import Graphics.UI.WX
 
 import SDLClient
 import Common
 import ChessTypes
 
 
+main :: IO ()
+main = withSocketsDo . start $ do
+
+  gamesMVar <- newMVar []
+
+  f <- frame [ text := "Pwning Pawns" ]
+  connectionInfo <- staticText f [ text := "You are not connected." ]
+  connectBtn <- button f []
+  gamesListBox <- singleListBox f [ items := [] ]
+  createGameBtn <- button f [ text := "Create game" ]
+  joinGameBtn <- button f [ text := "Join game" ]
+  quitBtn <- button f [ text := "Quit" ]
+  set f [ layout :=  column 0 [ hfill $ row 0 [ floatCenter $ widget connectionInfo, widget connectBtn ],
+                                fill $ widget gamesListBox,
+                                hfill . row 0 $ map (fill . widget) [ createGameBtn, joinGameBtn, quitBtn ] ] ]
+
+  let connectBtnProps = [ text := "Connect", on command := onConnectCommand ]
+      onConnectCommand = do
+        ip <- textDialog f "Server address :" "Connect to server" ""
+        maybeHandle <- connectToServer ip
+        case maybeHandle of
+          Nothing -> return ()
+          Just handle -> do
+            forkIO $ handleMessagesWithHeader "GAMESLIST" handle (refreshGamesList gamesMVar)
+            set connectionInfo [ text := "Connected to server " ++ ip ++ "."]
+            set connectBtn disconnectBtnProps
+
+      disconnectBtnProps = [ text := "Disconnect", on command := onDisconnectCommand ]
+      onDisconnectCommand = do
+        set connectionInfo [ text := "You are not connected" ]
+        set connectBtn connectBtnProps 
+
+  set connectBtn connectBtnProps
+
+  set quitBtn [ on command := close f ]
+  set createGameBtn [ on command := return () ]
+  set joinGameBtn [ on command := return () ]
+  
+  let periodicRefresh = do
+        withMVar gamesMVar $ \gamesList ->
+            set gamesListBox [ items := gamesList ]
+        yield
+  timer f [ interval := 25, on command := periodicRefresh ]
+
+
+connectToServer :: String -> IO (Maybe Handle)
+connectToServer serverAddress = do
+  handle <- connectTo serverAddress (PortNumber 28406)
+  connectionError <- hIsClosed handle
+  unlessMaybe connectionError $ do
+    hSetBuffering handle LineBuffering
+    hPutStrLn handle "CONNECT"
+    response <- hGetLine handle
+    unlessMaybe (response /= "CONNECTOK") $ do
+      return $ Just handle
+  
+unlessMaybe :: (Monad m) => Bool -> m (Maybe a) -> m (Maybe a)
+unlessMaybe True  _   = return Nothing
+unlessMaybe False act = act
+
+
+messageStream :: Handle -> IO [String]
+messageStream handle = lines <$> hGetContents handle
+
+handleMessagesWithHeader :: String -> Handle -> (String -> IO ()) -> IO ()
+handleMessagesWithHeader header handle act =
+    mapM_ act . mapMaybe (stripPrefix (header ++ " ")) =<< messageStream handle 
+
+
+refreshGamesList :: MVar [String] -> String -> IO ()
+refreshGamesList gamesMVar str = do
+  modifyMVar_ gamesMVar $ const (return $ split '\t' str)
+    where split _     [] = []
+          split delim cs = case span (/= delim) cs of
+                             (prefix, []) -> [prefix]
+                             (prefix, (_:rest)) -> prefix : split delim rest
+
+
+{-
 main :: IO ()
 main = withSocketsDo $ do
   putStr "Server: " >> hFlush stdout
@@ -43,4 +126,6 @@ recieveMoves :: Handle -> Chan String -> IO ()
 recieveMoves handle chan = forever $ do
   str <- hGetLine handle
   when ("MOVE " `isPrefixOf` str) $ writeChan chan (drop 5 str)
+
+-}
 
--- Server.hs
+++ Server.hs
@@ -1,32 +1,84 @@
+import Control.Concurrent
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Reader
 import Data.List
 import System.Exit
 import System.IO
 import Network
 
+------------------------------------------------------
+
+data AppData = AppData {
+      clientsMVar :: MVar [Handle],
+      gamesMVar :: MVar [String]
+    }
+
+type AppMonad a = ReaderT AppData IO a
+
+forkAppMonad :: AppMonad () -> AppMonad ThreadId
+forkAppMonad act = liftIO . forkIO . runReaderT act =<< ask
+
+------------------------------------------------------
+
+
+main :: IO ()
 main = withSocketsDo $ do
+  mvar1 <- newMVar []
+  mvar2 <- newMVar ["teletubbies", "tinky winky", "dipsy", "lala", "po"]
   socket <- listenOn (PortNumber 28406)
-  whiteHandle <- acceptClient socket "W"
-  blackHandle <- acceptClient socket "B"
-  hPutStrLn whiteHandle "START"
-  hPutStrLn blackHandle "START"
-  forever $ do
-    hPutStrLn blackHandle =<< hGetLine whiteHandle
-    hPutStrLn whiteHandle =<< hGetLine blackHandle
-    disconnected <- or <$> mapM hIsClosed [whiteHandle, blackHandle]
-    when disconnected exitSuccess
+  runReaderT (acceptClients socket) (AppData { clientsMVar = mvar1, gamesMVar = mvar2 })
 
-acceptClient socket response = do
-  (newClientHandle, _, _) <- accept socket
-  hSetBuffering newClientHandle LineBuffering
-  msg <- hGetLine newClientHandle
-  if ("CONNECT" `isPrefixOf` msg)
-    then do
-      hPutStrLn newClientHandle $ "ACCEPT " ++ response
-      return newClientHandle
-    else do
-      hClose newClientHandle
-      acceptClient socket response
+acceptClients :: Socket -> AppMonad ()
+acceptClients socket = forever $ do
+  (newClientHandle, _, _) <- liftIO $ accept socket
+  liftIO $ hSetBuffering newClientHandle LineBuffering
+  acceptClient newClientHandle
 
-      
+acceptClient :: Handle -> AppMonad ()
+acceptClient newClientHandle = do
+  msg <- liftIO $ hGetLine newClientHandle
+  case words msg of
+    ("CONNECT":_) -> do
+      addClient newClientHandle
+      liftIO $ hPutStrLn newClientHandle "CONNECTOK"
+      sendGamesList newClientHandle
+    _ -> do
+      liftIO $ hClose newClientHandle
+
+addClient :: Handle -> AppMonad ()
+addClient newClientHandle = do
+  cmv <- asks clientsMVar
+  liftIO . modifyMVar_ cmv $ \clients ->
+      return (newClientHandle:clients)
+  forkAppMonad $ recvMsgLoop newClientHandle
+  return ()
+
+recvMsgLoop :: Handle -> AppMonad ()
+recvMsgLoop clientHandle = do
+  mapM_ (processMsg clientHandle) . lines =<< (liftIO $ hGetContents clientHandle)
+  deleteClient clientHandle
+
+deleteClient :: Handle -> AppMonad ()
+deleteClient clientHandle = do
+  cmv <- asks clientsMVar
+  liftIO . modifyMVar_ cmv $ \clients ->
+      return (delete clientHandle clients)
+  liftIO $ hClose clientHandle
+
+processMsg :: Handle -> String -> AppMonad ()
+processMsg senderHandle msg =
+    case words msg of
+      _ -> return ()
+--      ["CREATEGAME", gameName] -> createGame gameName senderHandle
+--      _ -> undefined
+
+createGame :: String -> Handle -> AppMonad ()
+createGame = undefined
+
+sendGamesList :: Handle -> AppMonad ()
+sendGamesList handle = do
+  gmv <- asks gamesMVar
+  liftIO . withMVar gmv $ \gamesList ->
+      hPutStrLn handle $ "GAMESLIST " ++ intercalate "\t" gamesList ++ "\n"
+
